5cf453eb6449a7e6f4a0cd43ac4a5d143f9a8323
webfwd <webfwd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-03 22:24:00 +0000

    1.0.1 Few fixes in the documentation
    git-svn-id: https://plugins.svn.wordpress.org/mail-subscribe-list/tags/1.0.1@633688 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,34 @@
+=== Mail Subscribe List ===
+Contributors: webfwd
+Donate link: 
+Tags: mail, email, newsletter, subscribe, list, mailinglist, mail list, mailing list
+Requires at least: 3.0
+Tested up to: 3.4
+Stable tag: 
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin allows users to enter their name and email address to subscribe to a list which is available to view and export in the wordpress admin.
+
+== Description ==
+
+This plugin allows users to enter their name and email address on a simple unstyled form to subscribe to a simple mailing list which is available to view and modify in the wordpress admin area. 
+
+The subscribe form can be displayed on any wordpress page using the shortcode [smlsubform] or from your wordpress theme by calling the php function <?php echo smlsubform(); ?>.
+
+I developed this plugin as I could not find any plugin that simply allows users to submit their name and email address to a simple list viewable in the wordpress admin, all the plugins that I found had lots of extra features such as sending out mass emails and double opt-in which my clients do not need.
+
+== Installation ==
+
+1. Upload `mail-subscribe-list` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Place `[smlsubform]` in any of your pages or <?php echo smlsubform(); ?> in your templates
+
+== Screenshots ==
+
+1. http://www.webfwd.co.uk/wp-plugins/mail-subscribe-list.jpg
+
+== Changelog ==
+
+1.0.1		Few fixes in the documentation
+1.0		 Developed Mail Subscribe List Plugin
---
+++ sml.php
@@ -0,0 +1,84 @@
+<?php
+/*
+Plugin Name: Mail Subscribe List
+Plugin URI: http://www.webfwd.co.uk/wp-plugins/mail-subscribe-list.php
+Description: This plugin allows users to enter their name and email address to subscribe to a list which is available to view and export in the wordpress admin.
+Version: 1.0.1
+Author: Richard Leishman t/a Webforward
+Author URI: http://www.webfwd.co.uk/
+License: GPL
+
+
+Copyright 2012 Richard Leishman t/a Webforward  (email : richard@webfwd.co.uk)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+GNU General Public License: http://www.gnu.org/licenses/gpl.html
+
+*/
+
+define(PLUGIN_NAME, "Mail Subscribe List");
+define(PLUGIN_VER, "1.0.1");
+
+// Plugin Activation
+function sml_install()
+{
+    global $wpdb;
+    $table = $wpdb->prefix."sml";
+    $structure = "CREATE TABLE $table (
+        id INT(9) NOT NULL AUTO_INCREMENT,
+        sml_name VARCHAR(200) NOT NULL,
+        sml_email VARCHAR(200) NOT NULL,
+	UNIQUE KEY id (id)
+    );";
+    $wpdb->query($structure);
+}
+register_activation_hook( __FILE__, 'sml_install' );
+
+// Left Menu Button
+function register_sml_menu() {
+	add_menu_page('Subscribers', 'Subscribers', 'add_users', dirname(__FILE__).'/index.php', '',   plugins_url('sml-admin-icon.png', __FILE__), 58);
+}
+add_action('admin_menu', 'register_sml_menu');
+
+// Generate Subscribe Form 
+
+
+function smlsubform($atts){
+	extract(shortcode_atts(array(
+		"prepend" => '',  
+        "showname" => true,
+		"nametxt" => 'Name:',
+		"emailtxt" => 'Email:',
+		"submittxt" => 'Submit'
+    ), $atts));
+	
+	$return = '<form class="sml_subscribe" method="post"><input name="sml_subscribe" type="hidden" value="1">';
+	if ($prepend) $return .= '<p class="prepend">'.$prepend.'</p>';
+	if ($showname) $return .= '<p class="sml_name"><label for="sml_name">'.$nametxt.'</label><input class="" placeholder="Name…" name="sml_name" type="text" value=""></p>';
+	$return .= '<p class="sml_email"><label for="sml_email">'.$emailtxt.'</label><input class="" name="sml_email" placeholder="Email…" type="text" value=""></p>';
+	$return .= '<p class="sml_submit"><input name="submit" class="btn sml_submitbtn" type="submit" value="'.$submittxt.'"></p>';
+	$return .= '</form>';
+ 	return $return;
+}
+add_shortcode( 'smlsubform', 'smlsubform' );
+
+// Handle form Post
+if ($_POST['sml_subscribe']) {
+	$name = $_POST['sml_name'];
+	$email = $_POST['sml_email'];
+	if (is_email($email)) {
+		$wpdb->query("insert into ".$wpdb->prefix."sml (sml_name, sml_email) values ('".$wpdb->escape($name)."', '".$wpdb->escape($email)."')");
+		
+	}
+}
+
+
+?>
